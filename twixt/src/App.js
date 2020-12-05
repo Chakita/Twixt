b4f4e7b929c8bb94b831192da1c9c0c3d6f51b57
pratheek3010 <68456928+pratheek3010@users.noreply.github.com>
2020-12-05 08:34:16 +0530

    Update App.js

--- twixt/src/App.js
+++ twixt/src/App.js
@@ -2,48 +2,42 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ChatRoom from "./ChatRoom/ChatRoom";
 import InterestPage from "./interest.js";
-//import SignUp from "./signup.js";
+import SignUp from "./signup.js"
 import HomePage from "./homepage.js";
 import Login from "./login.js";
-//import Chatroom from "./Chatroom.js";
+//import Chatroom from "./chatroom.js";
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return <Login />;
-            }}
-          />
-
-          <Route
-            path="/search"
-            render={() => {
-              return <InterestPage />;
-            }}
-          />
-          <Route
-            path="/discussion"
-            component={ChatRoom}
-            /*path="/discussion"
-            render={() => {
-              return <Chatroom />;
-            }}*/
-          />
-          <Route
-            path="/home"
-            render={() => {
-              return <HomePage />;
-            }}
-          />
-        </Router>
-      </React.Fragment>
-    );
+class App extends React.Component{
+  render(){
+        return(
+          <Router>
+          <Route exact path="/" render={
+            ()=>{
+                return(<Login/>)
+                }
+            }/>
+          <Route path="/signup" render={
+            ()=>{
+              return(<SignUp/>)
+            }
+          }/>
+          <Route path="/search" render={
+              ()=>{
+                return(<InterestPage/>)}
+            }/>
+          <Route path="/discussion" render={
+              ()=>{
+                  return(<ChatRoom/>)}
+              }/>
+          <Route path="/home" render={
+            ()=>{
+              return(<HomePage/>)
+            }
+          }/>
+        </Router>   
+        );
   }
 }
 
 export default App;
+
