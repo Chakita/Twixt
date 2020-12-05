dcafec2cbe5c1ae6a60aae1ce64fb07383c741ff
pratheek3010 <68456928+pratheek3010@users.noreply.github.com>
2020-12-05 08:44:32 +0530

    Update signup.js

--- twixt/src/signup.js
+++ twixt/src/signup.js
@@ -1,76 +1,50 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import "./styling/signup.css";
+import React , {useState} from "react";
+import {useHistory} from "react-router-dom";
+import axios from 'axios';
+import "./styling/signup.css"
 
-/*const SignUp = () => {
-  const [mail, sete] = useState("");
-  const [pass, setpass] = useState("");
+const SignUp=()=>{
+    const [mail , sete]=useState('');
+    const [pass , setpass]=useState('');
+    const history=useHistory();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    //const history=useHistory();
+    function handleSubmit(e){
+        e.preventDefault();
 
-    let user_data = {
-      email: mail,
-      password: pass,
-    };
-    axios
-      .post("http://localhost:8080/signup", user_data)
-      .then((resp) => {
-        if (resp) {
-          //history.push("/search");
-          alert("Successful register");
-        } else {
-          alert("Unsuccessful registration");
+        let user_data={
+            email:mail,
+            password:pass,
         }
-      })
-      .catch((err) => console.log(err));
-  }
-  axios
-    .post("http://localhost:8080/signup", user_data)
-    .then((resp) => {
-      if (resp) {
-        history.push("/search");
-      } else {
-        alert("Unsuccessful registration");
-      }
-    })
-    .catch((err) => console.log(err));
-};
+        axios.post("http://localhost:8080/signup" , user_data)
+        .then((resp)=>{
+                if(resp){
+                    history.push("/");
+                }
+                else{
+                    alert("Unsuccessful registration");
+                }
+        })
+        .catch(err=>console.log(err));
+    }
 
-return (
-    <div className="wrapper">
-      <form onSubmit={(event) => handleSubmit(event)} className="form-wrapper">
-        <h1>Sign Up</h1>
-        <div className="email">
-          <label htmlFor="email">Enter Email:</label>
-          <input
-            required
-            type="email"
-            name="email"
-            value={mail}
-            id="email"
-            onChange={(event) => sete(event.target.value)}
-          />
+    return(
+        <div className="wrapper">
+            <form onSubmit={(event)=>handleSubmit(event)} className="form-wrapper">
+                <h1>Sign Up</h1>
+                <div className="email">
+                    <label htmlFor="email">Enter Email:</label>
+                    <input required type="email" name="email" value={mail} id="email" onChange={(event)=>sete(event.target.value)}/>
+                </div>
+                <div className="password">
+                    <label htmlFor="pwd">Enter Password:</label>
+                    <input required type="password" name="password" value={pass} id="password" onChange={(event)=>setpass(event.target.value)}/>
+                </div>
+                <div className="signin">
+                    <button type="submit">Sign Up</button>
+                </div>
+            </form>
         </div>
-        <div className="password">
-          <label htmlFor="pwd">Enter Password:</label>
-          <input
-            required
-            type="password"
-            name="password"
-            value={pass}
-            id="password"
-            onChange={(event) => setpass(event.target.value)}
-          />
-        </div>
-        <div className="signin">
-          <button type="submit">Sign Up</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+    );
+}
 
-export default SignUp;*/
+export default SignUp;
