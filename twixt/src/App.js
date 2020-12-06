1e5763a70416359417f2241bfcb4111836cf3a71
chakita <chakitapesu@gmail.com>
2020-12-06 17:21:25 +0530

    Added People component

--- twixt/src/App.js
+++ twixt/src/App.js
@@ -2,42 +2,56 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ChatRoom from "./ChatRoom/ChatRoom";
 import InterestPage from "./interest.js";
-import SignUp from "./signup.js"
+import SignUp from "./signup.js";
 import HomePage from "./homepage.js";
 import Login from "./login.js";
+import People from "./people.js";
 //import Chatroom from "./chatroom.js";
 
-class App extends React.Component{
-  render(){
-        return(
-          <Router>
-          <Route exact path="/" render={
-            ()=>{
-                return(<Login/>)
-                }
-            }/>
-          <Route path="/signup" render={
-            ()=>{
-              return(<SignUp/>)
-            }
-          }/>
-          <Route path="/search" render={
-              ()=>{
-                return(<InterestPage/>)}
-            }/>
-          <Route path="/discussion" render={
-              ()=>{
-                  return(<ChatRoom/>)}
-              }/>
-          <Route path="/home" render={
-            ()=>{
-              return(<HomePage/>)
-            }
-          }/>
-        </Router>   
-        );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          path="/signup"
+          render={() => {
+            return <SignUp />;
+          }}
+        />
+        <Route
+          path="/search"
+          render={() => {
+            return <InterestPage />;
+          }}
+        />
+        <Route
+          path="/discussion"
+          render={() => {
+            return <ChatRoom />;
+          }}
+        />
+        <Route
+          path="/home"
+          render={() => {
+            return <HomePage />;
+          }}
+        />
+        <Route
+          path="/people"
+          render={() => {
+            return <People />;
+          }}
+        />
+      </Router>
+    );
   }
 }
 
 export default App;
-
